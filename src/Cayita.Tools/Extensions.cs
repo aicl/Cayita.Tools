e8204eb45c66b122dfe2d639ddf3bcd6a2264524
angel ignacio colmenares laguado <angel.ignacio.colmenares@gmail.com>
2014-01-03 01:10:37 -0500

    Update Extensions.cs

--- src/Cayita.Tools/Extensions.cs
+++ src/Cayita.Tools/Extensions.cs
@@ -11,6 +11,9 @@ using ServiceStack.ServiceInterface;
 using ServiceStack.Text;
 using System.Linq.Expressions;
 using ServiceStack.Redis;
+using System.Text;
+using System.Security.Cryptography;
+
 
 namespace System.Collections.Generic
 {
@@ -185,4 +188,88 @@ namespace ServiceStack.ServiceInterface{
 			}
 		}
 	}
+}
+
+namespace System
+{
+	public static partial class Extensions
+	{
+
+		public static string Encriptar(this string texto, string key)
+		{
+			if(string.IsNullOrEmpty(texto)) return "";
+
+			//arreglo de bytes donde guardaremos la llave
+			byte[] keyArray;
+			//arreglo de bytes donde guardaremos el texto
+			//que vamos a encriptar
+			byte[] Arreglo_a_Cifrar = UTF8Encoding.UTF8.GetBytes(texto);
+			
+			//se utilizan las clases de encriptaciÃ³n
+			//provistas por el Framework
+			//Algoritmo MD5
+			MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+			//se guarda la llave para que se le realice
+			//hashing
+			keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+			
+			hashmd5.Clear();
+			
+			//Algoritmo 3DAS
+			TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+			
+			tdes.Key = keyArray;
+			tdes.Mode = CipherMode.ECB;
+			tdes.Padding = PaddingMode.PKCS7;
+			
+			//se empieza con la transformaciÃ³n de la cadena
+			ICryptoTransform cTransform = tdes.CreateEncryptor();
+			
+			//arreglo de bytes donde se guarda la
+			//cadena cifrada
+			byte[] ArrayResultado = cTransform.TransformFinalBlock(Arreglo_a_Cifrar,0, Arreglo_a_Cifrar.Length);
+			
+			tdes.Clear();
+			
+			//se regresa el resultado en forma de una cadena
+			return Convert.ToBase64String(ArrayResultado,0, ArrayResultado.Length);
+		}
+		
+		public static string Desencriptar(this string textoEncriptado, string key){
+
+			if(string.IsNullOrEmpty(textoEncriptado)) return "";
+
+			byte[] keyArray;
+			//convierte el texto en una secuencia de bytes
+			byte[] Array_a_Descifrar = Convert.FromBase64String(textoEncriptado);
+			
+			//se llama a las clases que tienen los algoritmos
+			//de encriptaciÃ³n se le aplica hashing
+			//algoritmo MD5
+			MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+			
+			keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+			
+			hashmd5.Clear();
+			
+			TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+			
+			tdes.Key = keyArray;
+			tdes.Mode = CipherMode.ECB;
+			tdes.Padding = PaddingMode.PKCS7;
+			
+			ICryptoTransform cTransform = tdes.CreateDecryptor();
+			
+			byte[] resultArray = cTransform.TransformFinalBlock(Array_a_Descifrar,0, Array_a_Descifrar.Length);
+			
+			tdes.Clear();
+			//se regresa en forma de cadena
+			return UTF8Encoding.UTF8.GetString(resultArray);
+		}
+		
+		//
+		
+	}
+	
+
 }
