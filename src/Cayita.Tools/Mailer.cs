f32a2646cd3c6c4234de9d2219b3b36da0898761
angel ignacio colmenares laguado <angel.ignacio.colmenares@gmail.com>
2014-01-03 01:06:29 -0500

    Update Mailer.cs

--- src/Cayita.Tools/Mailer.cs
+++ src/Cayita.Tools/Mailer.cs
@@ -9,28 +9,58 @@ namespace Cayita.Tools
 	public class Mailer
 	{
 		SmtpClient SmtpServer {get ;set;}
-		
-		public Mailer ( string server, int port, string user, string password,bool ssl=true)
+
+		public string Server {private get; set;}
+		public string User { get; set;}
+		public int Port {private get; set;}
+		public string Password {private get; set;}
+		public bool EnableSsl {private get; set;}
+
+		public  Mailer(){}
+
+		public Mailer ( string server, int port, string user, string password,bool enableSsl=true)
 		{
-			
-			SmtpServer = new SmtpClient(server);
-			SmtpServer.Port = port;
-			SmtpServer.Credentials = 
-				new NetworkCredential(user, password);
-			SmtpServer.EnableSsl = ssl;
+
+			Server=server;
+			User= user;
+			Password=password;
+			EnableSsl= enableSsl;
+			Port= port;
+			Authenticate();
+		}
+
+		private void Authenticate()
+		{
+	
+			SmtpServer = new SmtpClient(Server,Port);
+			SmtpServer.Credentials = 	new NetworkCredential(User, Password);
+			SmtpServer.EnableSsl = EnableSsl;
+
 			ServicePointManager.ServerCertificateValidationCallback =
-				delegate(object s, X509Certificate certificate,
-				         X509Chain chain, SslPolicyErrors sslPolicyErrors)
-			{ return true; };
-			
+				delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+					{ return true; };
 		}
-		
-		
+
+	
 		public void Send(Action<MailMessage> mail ){
-			MailMessage message = new MailMessage();
-			mail(message);
-			SmtpServer.Send(message);
+
+			if(SmtpServer== null) Authenticate();
+
+			using (var message = new MailMessage())
+			{
+				message.From= new MailAddress(User, User); 
+				mail(message);
+				SmtpServer.Send(message);
+			}
+
 		}
-		
+
+		public void Reset()
+		{
+			if( SmtpServer!=null){
+				SmtpServer.Dispose();
+				SmtpServer=null;
+			}
+		}
 	}
 }
